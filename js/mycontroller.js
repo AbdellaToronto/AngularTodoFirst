1dfa8959689955b3efd2e1d84f93ac2f8011aa9e
Abdella <abdellaalioncan@gmail.com>
2013-07-30 02:03:22 -0400

    Got firebase tied up, styling needs a LOT of work. Yuck.

--- js/mycontroller.js
+++ js/mycontroller.js
@@ -5,3 +5,22 @@
  * Time: 1:45 AM
  * To change this template use File | Settings | File Templates.
  */
+
+Todo.controller('mycontroller',['$scope','angularFire',
+
+    function mycontroller($scope, angularFire){
+
+var url = "https://dellas.firebaseio.com/todo";
+var promise = angularFire(url, $scope, 'todoList');
+
+        promise.then(function(todoList){
+            $scope.save = function(){
+                $scope.todoList.push({x:$scope.myTask});
+
+                $scope.myTask = '';
+            };
+
+        })
+    }
+
+])
