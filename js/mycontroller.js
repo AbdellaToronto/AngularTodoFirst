8ef64b3c409b7bd347c779c3e33f91cb6b81b185
Abdella <abdellaalioncan@gmail.com>
2013-07-31 00:17:13 -0400

    added delete functionality

--- js/mycontroller.js
+++ js/mycontroller.js
@@ -6,22 +6,27 @@
  * To change this template use File | Settings | File Templates.
  */
 
-Todo.controller('mycontroller',['$scope','angularFire',
+Todo.controller('mycontroller', ['$scope', 'angularFire',
 
-    function mycontroller($scope, angularFire){
+    function mycontroller($scope, angularFire) {
 
-var url = "https://dellas.firebaseio.com/todo"; //url of todolist at firebase
-var promise = angularFire(url, $scope, 'todoList'); //promise, last argument is referencing the firebase model
+        var url = "https://dellas.firebaseio.com/todo"; //url of todolist at firebase
+        var promise = angularFire(url, $scope, 'todoList'); //promise, last argument is referencing the firebase model
 
-        promise.then(function(todoList){
+        promise.then(function (todoList) {
 
-                $scope.save = function(){    //when you click the save button
-                    if($scope.todoForm.$valid){
-                        $scope.todoList.push({x:$scope.myTask});    //pushes the data in 'myTask' into the html list
+            $scope.save = function () {    //when you click the save button
+                if ($scope.todoForm.$valid) {
+                    $scope.todoList.push({x: $scope.myTask});    //pushes the data in 'myTask' into the html list
 
-                 $scope.myTask = ''; //clears the value in myTask
+                    $scope.myTask = ''; //clears the value in myTask
                 }
             };
+
+            $scope.delete = function(y) {
+                $scope.todoList.splice($scope.todoList.indexOf(y), 1);
+            }
+
         })
     }
-])
+]);
