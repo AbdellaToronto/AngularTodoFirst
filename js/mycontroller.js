ad6dc1fce536fc6249bf985dc2c1ec1afacb2284
Abdella <abdellaalioncan@gmail.com>
2013-07-30 23:50:41 -0400

    Changed the list type to take advantage of boostrap 3, added some responsive styling

--- js/mycontroller.js
+++ js/mycontroller.js
@@ -10,17 +10,18 @@ Todo.controller('mycontroller',['$scope','angularFire',
 
     function mycontroller($scope, angularFire){
 
-var url = "https://dellas.firebaseio.com/todo";
-var promise = angularFire(url, $scope, 'todoList');
+var url = "https://dellas.firebaseio.com/todo"; //url of todolist at firebase
+var promise = angularFire(url, $scope, 'todoList'); //promise, last argument is referencing the firebase model
 
         promise.then(function(todoList){
-            $scope.save = function(){
-                $scope.todoList.push({x:$scope.myTask});
 
-                $scope.myTask = '';
+                $scope.save = function(){    //when you click the save button
+                    if($scope.todoForm.$valid){
+                        $scope.todoList.push({x:$scope.myTask});    //pushes the data in 'myTask' into the html list
+
+                 $scope.myTask = ''; //clears the value in myTask
+                }
             };
-
         })
     }
-
 ])
